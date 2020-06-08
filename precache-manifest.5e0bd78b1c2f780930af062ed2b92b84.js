2e961a1520f9aa35135e7799cb89180202b105ec
ThuyN <Thuynguyen.nht20@gmail.com>
2020-06-07 23:19:20 -0700

    Updates

--- precache-manifest.5e0bd78b1c2f780930af062ed2b92b84.js
+++ precache-manifest.5e0bd78b1c2f780930af062ed2b92b84.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a894e4fd605891a793bb39fdd01d85ad",
+    "revision": "c851ad3b3287406a50123a3e12db98fb",
     "url": "/clicking-memory/index.html"
   },
   {
-    "revision": "4b37b5d5a069cc36601e",
-    "url": "/clicking-memory/static/css/main.64de596b.chunk.css"
+    "revision": "c7d746f4d6be20182486",
+    "url": "/clicking-memory/static/css/main.b41a99da.chunk.css"
   },
   {
     "revision": "21ad76d7105ccc888dc0",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clicking-memory/static/js/2.c755bf04.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4b37b5d5a069cc36601e",
+    "revision": "c7d746f4d6be20182486",
     "url": "/clicking-memory/static/js/main.196fffcc.chunk.js"
   },
   {
